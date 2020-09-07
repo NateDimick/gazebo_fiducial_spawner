28d42efe288d7911edb2bd972113ad6ec007cc46
NateDimick <natdimick@gmail.com>
2020-09-07 10:39:53 -0400

    nicer model file templates

--- src/fiducial_spawner.py
+++ src/fiducial_spawner.py
@@ -27,31 +27,25 @@ def new_model(fid_id, fid_dict, size=0):
     cv2.imwrite(get_nearby_file("../models/arucotag{0}/arucotag{0}.png".format(fid_id)), img)
     # make changes to sdf file
     with open(get_nearby_file("../models/arucotag{}/arucotag.sdf".format(fid_id)), 'r+') as sdf:
-        lines = sdf.readlines()
-        lines[1] = "  <model name='arucotag{}'>\n".format(fid_id)
-        if size:
-            lines[20] = "            <size>{0} {0} 0.0001</size>\n".format(size)
-        lines[25] = "           <uri>file://models/arucotag{0}/arucotag{0}.material</uri>\n".format(fid_id)
-        lines[26] = "           <name>arucotag{}</name>\n".format(fid_id)
-        sdf.seek(0)
-        for line in lines:
-            sdf.write(line)
+        # this pattern is generally the same for all three files
+        output = sdf.read().format(fid_id, size if size else 0.15)  # read file and fill placeholders
+        sdf.seek(0)  # seek back to start of file
+        sdf.write(output)  # write new formatted file
+        sdf.truncate()  # truncate any leftover text from template after write
     # make changes to config file
     with open(get_nearby_file("../models/arucotag{}/arucotag.config".format(fid_id)), 'r+') as config:
-        lines = config.readlines()
-        lines[2] = "  <name>arucotag{}</name>\n".format(fid_id)
-        lines[4] = "  <sdf version='1.5'>arucotag{}.sdf</sdf>\n".format(fid_id)
+        output = config.read().format(fid_id)
         config.seek(0)
-        for line in lines: 
-            config.write(line)
+        config.write(output)
+        config.truncate()
     # make changes to material file
     with open(get_nearby_file("../models/arucotag{}/arucotag.material".format(fid_id)), 'r+') as mat:
-        lines = mat.readlines()
-        lines[0] = "material arucotag{}\n".format(fid_id)
-        lines[8] = "        texture arucotag{}.png\n".format(fid_id)
+        output = mat.read().format(fid_id)
+        output = output.replace("[", "{")  # the material file needs to have curly braces, but the template has sqare bracket placeholders for python string formatting to work
+        output = output.replace("]", "}")
         mat.seek(0)
-        for line in lines:
-            mat.write(line)
+        mat.write(output)
+        mat.truncate()
     # rename files
     rename(get_nearby_file("../models/arucotag{}/arucotag.sdf".format(fid_id)), get_nearby_file("../models/arucotag{0}/arucotag{0}.sdf".format(fid_id)))
     rename(get_nearby_file("../models/arucotag{}/arucotag.config".format(fid_id)), get_nearby_file("../models/arucotag{0}/arucotag{0}.config".format(fid_id)))
